e3772b7ea73f6539295239c458dcfc5a8199d4b5
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-01 21:34:00 +0530

    print 1to n and n to 1

---
+++ Recursion/print1toN.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+void print1toN(int n){
+    
+    if(n==0){
+        return;
+    }
+
+    print1toN(n-1);
+    cout<<n<<" ";
+ 
+
+
+}
+
+int main(){
+
+
+int n=4;
+
+print1toN(4);
+
+
+}
