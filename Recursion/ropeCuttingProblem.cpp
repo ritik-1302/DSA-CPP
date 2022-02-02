cd78de88247dee3c53decc49763460b00f3e6056
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-02 21:42:21 +0530

    bug fix

--- Recursion/ropeCuttingProblem.cpp
+++ Recursion/ropeCuttingProblem.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+
+
+int compare(int a,int b,int c){
+    
+    if(a>b&&a>c){
+        return a;
+
+    }else if(b>a&&b>c){
+        return b;
+
+    }
+    else{
+        return c; 
+    }
+
+
+}
+
+
 int maxPiece(int n,int a,int b,int c){
 
 if(n==0){
@@ -11,7 +30,7 @@ if(n<0){
     return -1;
 
 }
-int res=max(maxPiece(n-a,a,b,c),maxPiece(n-b,a,b,c),maxPiece(n-c,a,b,c));
+int res=compare(maxPiece(n-a,a,b,c),maxPiece(n-b,a,b,c),maxPiece(n-c,a,b,c));
 
 if(res==-1){
     return -1;
@@ -21,7 +40,16 @@ if(res==-1){
 }
 
 return res+1;
+}
 
+int main(){
+
+int n=23,a=11,b=9,c=12;
+
+int res=maxPiece(n,a,b,c);
+
+cout<<res;
 
 
 }
+
