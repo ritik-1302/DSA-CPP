f83dad975fcab97fd5d3706fddd12b87929bb323
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-02 21:59:03 +0530

    genrate subsets

---
+++ Recursion/genrateSubsets.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+ 
+ void subsets(string &s,string curr="",int i=0){
+     if(i==s.length()){
+         cout<<curr<<endl;
+         return;
+
+     }
+
+     subsets(s,curr,i+1);
+     subsets(s,curr+s[i],i+1);
+
+ }
+
+
+ int main(){
+
+  
+  string s="ABC";
+  subsets(s);
+
+
+
+ }
