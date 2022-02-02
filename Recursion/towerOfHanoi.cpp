19018ff7703147b05bd5c701d4c37d93685c5453
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-02 22:32:21 +0530

    tower of hanoi

---
+++ Recursion/towerOfHanoi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n,char A,char B,char C){
+    if(n==1){
+        cout<<"Move 1 from "<<A<<" to "<<C<<endl;
+        return;
+    }
+    towerOfHanoi(n-1,A,C,B);
+    cout<<"Move "<<n<<" from"<<A<<" to "<<C<<endl;
+    towerOfHanoi(n-1,B,A,C);
+
+
+}
+
+int main(){
+
+towerOfHanoi(4,'A','B','C');
+
+
+}
