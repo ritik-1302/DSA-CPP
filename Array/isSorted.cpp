d46db4d2fe453927f5c57d50266faf5bd6d48c65
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-09 22:02:31 +0530

    isSorted

---
+++ Array/isSorted.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+bool isSorted(int arr[],int n){
+
+    for(int i=1;i<n;i++){
+        if(arr[i]<arr[i-1]){
+            return false;
+
+        }
+    }
+
+    return true;
+
+
+}
+
+int main(){
+
+    int arr[]={
+        1,2,3,4,5
+    };
+    
+    cout<<isSorted(arr,5);
+    
+
+}
