1d4959df47fb4afea5ba15ce9e14320684b2f202
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-12 22:54:41 +0530

    min Group Flips

---
+++ Array/minGroupflips.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void printGroups(bool arr[],int n){
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1]){
+            if(arr[i]!=arr[0]){
+                cout<<"From "<<i<<" to";
+
+
+            }else{
+                cout<<i-1<<endl;
+
+            }
+        }
+    }
+    if(arr[n-1]!=0){
+        cout<<n-1<<endl;
+
+    }
+}
+
+int main(){
+
+    bool arr[]={
+
+        0,0,1,1,0,0,1,1,0,0
+
+    };
+
+    printGroups(arr,10);
+
+
+
+}
