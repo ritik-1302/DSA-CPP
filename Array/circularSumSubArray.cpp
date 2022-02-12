8e269877da5981021dbed80008ffa84272092b7b
Ritik Shah <79132559+ritik-1302@users.noreply.github.com>
2022-02-12 22:30:49 +0530

    majority element

--- Array/circularSumSubArray.cpp
+++ Array/circularSumSubArray.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int normalMaxSum(int arr[],int n){
-    int res=arr[0];
-    int maxEnding=arr[0];
 
-    for(int i=0;i<n;i++){
+int normalMaxSum(int arr[], int n)
+{
+	int res = arr[0];
 
-        maxEnding=max(arr[i],maxEnding+arr[i]);
-        res=max(res,maxEnding);
+	int maxEnding = arr[0];
 
-    }
-    return res;
+	for(int i = 1; i < n; i++)
+	{
+		maxEnding = max(maxEnding + arr[i], arr[i]);
 
+		res = max(maxEnding, res);
+	}
+	
+	return res;
 }
 
-int overallMaxSum(int arr[],int n ){
-    int max_normal=normalMaxSum(arr,n);
-    if(max_normal<0){
-        return max_normal;
+int overallMaxSum(int arr[], int n)
+{
+	int max_normal = normalMaxSum(arr, n);
 
-    }
+	if(max_normal < 0)
+		return max_normal;
 
-    int arr_sum=0;
-    for(int i=0;i<n;i++){
-        arr_sum+=arr[i];
-        arr[i]=-arr[i];
-    }
-    int max_circular=arr_sum+normalMaxSum(arr,n);
+	int arr_sum = 0;
 
-    return max(max_normal,max_circular);
+	for(int i = 0; i < n; i++)
+	{
+		arr_sum += arr[i];
+
+		arr[i] = -arr[i];
+	}
+
+	int max_circular = arr_sum + normalMaxSum(arr, n);
+
+	return max(max_circular, max_normal);
+}
+
+
+
+int main() {
+	
+     int arr[] = {8, -4, 3, -5, 4}, n = 5;
+
+     cout<<overallMaxSum(arr, n);
     
-
 }
---
+++ Array/majorityElement.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int findMajority(int arr[],int n){
+    int res=0;
+    int count=1;
+
+    for(int i=0;i<n;i++){
+
+
+        if(arr[res]==arr[i]){
+            count++;
+        }else{
+            count--;
+
+        }
+
+
+        if(count==0){
+            res=i;
+            count=1;
+
+        }
+    }
+
+    count=0;
+    for(int i=0;i<n;i++){
+        if(arr[res]==arr[i]){
+            count++;
+            }
+
+    }
+    
+        if(count<=n/2){
+            res=-1;
+            
+        }
+
+    return res;
+
+
+}
+
+int main(){
+    int arr[]={
+        6,8,4,8,8
+
+    };
+
+    cout<<findMajority(arr,5);
+
+
+
+
+}
